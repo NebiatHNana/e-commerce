dff36955b2078b64d06393640149addc25a13af4
NebiatHNana <nebiat.nana@mail.utoronto.ca>
2023-02-16 17:17:10 -0500

    bug fix resume

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -4,7 +4,7 @@ import "./Resume.css"
 export default function Resume() {
     return (
         <div className="resume">
-        <h2>My Resume</h2>
+        <h2>My Resume!</h2>
         <a href="/about"><img src="/assets/Resume.png" alt="Resume"/></a>
     </div>     
     );
