6753233f3091c6ac44a2cabf996b48e5f4e6e11f
NebiatHNana <nebiat.nana@mail.utoronto.ca>
2023-02-16 17:14:55 -0500

    resume link bug fixed

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -5,8 +5,7 @@ export default function Resume() {
     return (
         <div className="resume">
         <h2>My Resume</h2>
-        <a className="download" href="/assets/Resume.zip">Download Resume</a>
-        <a href="/about"><img src="asset/resume.png" alt="Resume"/></a>
+        <a href="/about"><img src="/assets/Resume.png" alt="Resume"/></a>
     </div>     
     );
 }
