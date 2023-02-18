6a919c504e6ff991f2da2caa3fc5454dc25331ae
NebiatHNana <nebiat.nana@mail.utoronto.ca>
2023-02-17 19:25:19 -0500

    added files

--- src/components/Navbar/Header.jsx
+++ src/components/Navbar/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
     return (
       <div>
       <header>
-      <h1>Nebiat H Nana</h1>
+      <h1>On code</h1>
       <a href="/about"><img className="header-logo" src="/assets/logo.png" alt="Nebiat"/></a>
     
 <Navbar/>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,9 +4,9 @@ export default function Navbar() {
     return (
       <nav>
           <a href="/about">About</a>
-          <a href="/portfolio">Portfolio</a>
+          <a href="/portfolio">t-shirts</a>
           <a href="/contact">Contact</a>
-          <a href="/resume">Resume</a>
+          <a href="/resume">Login</a>
       </nav>
     );
   }
